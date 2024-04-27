c1d27246bf20b35178033825c26a0d12b26506c6
mauimaui <101412933+mauimaui17@users.noreply.github.com>
2024-04-28 02:43:22 +0800

    static

--- my_demand_tracker/settings.py
+++ my_demand_tracker/settings.py
@@ -131,7 +131,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 # Default primary key field type
